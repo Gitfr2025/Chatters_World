78c7189dfd9200801e4964abef19f81399a64932
Gitfr <Gitfr2025@gmail.com>
2024-05-13 19:22:19 +0200

    Added file structure

---
+++ Assets/_Code/Example_Script.cs
@@ -0,0 +1,10 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Example_Script : UdonSharpBehaviour
+{
+    [TextArea]
+    public string Notes = "Comment Here.";
+}
